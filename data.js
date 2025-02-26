4f476ad55798fa9aadeedfa1643314aafa9eb25d
ChislaineH <23099267@student.hhs.nl>
2025-02-26 09:49:03 +0100

    implementation song images

--- data.js
+++ data.js
@@ -4,222 +4,259 @@ const songs = [
     artist: "Culture Jam ft Bramsito, Frenna",
     year: 2022,
     duration: 2.36,
+    image: "/img/songs/culture-jam-ok.jpg"
   },
   {
     name: "Favorite Girl",
     artist: "Priceless",
     year: 2021,
     duration: 2.51,
+    image: "/img/songs/priceless-favorite-girl.jpg"
   },
   {
     name: "7 Dagen",
     artist: "Architrackz",
     year: 2022,
     duration: 3.15,
+    image: "/img/songs/architrackz-7-dagen.jpg"
   },
   {
     name: "Karma",
     artist: "Architrackz ft. KATNUF",
     year: 2024,
     duration: 2.52,
+    image: "/img/songs/architrackz-karma.jpg"
   },
   {
     name: "Lost & Found",
     artist: "Broederliefde",
     year: 2024,
     duration: 3.16,
+    image: "/img/songs/broederliefde-lost-and-found.jpg"
   },
   {
     name: "After Six",
     artist: "Broederliefde ft. Antoon, Ronnie Flex",
     year: 2024,
     duration: 2.46,
+    image: "/img/songs/broederliefde-after-six.jpg"
   },
   {
     name: "Guap",
     artist: "Bryan MG & Architrackz",
     year: 2023,
     duration: 2.41,
+    image: "/img/songs/bryan-mg-guap.jpg"
   },
   {
     name: "Big 7",
     artist: "Burna Boy",
     year: 2023,
     duration: 2.31,
+    image: "/img/songs/burna-boy-big-7.jpg"
   },
   {
     name: "Dumlla Dumlla",
     artist: "Dafina Zeqiri & DYSTINCT",
     year: 2024,
     duration: 3.07,
+    image: "/img/songs/dafina-zeqiri-dumlla-dumlla.jpg"
   },
   {
     name: "Geen Reden",
     artist: "Delany ft. Yxng Le",
     year: 2024,
     duration: 2.56,
+    image: "/img/songs/delany-geen-reden.jpg"
   },
   {
     name: "679",
     artist: "Fetty Wap ft. Monty",
     year: 2015,
     duration: 3.11,
+    image: "/img/songs/fetty-wap-679.jpg"
   },
   {
     name: "Girls Wanna Have Fun",
     artist: "Frenna",
     year: 2024,
     duration: 3.29,
+    image: "img/songs/frenna-girls-wanna-have-fun.jpg"
   },
   {
     name: "Maradona",
     artist: "Frenna",
     year: 2024,
     duration: 2.26,
+    image: "/img/songs/frenna-maradona.jpg"
   },
   {
     name: "Nog Steeds",
     artist: "Jayh",
     year: 2024,
     duration: 3.18,
+    image: "/img/songs/jayh-nog-steeds.jpg"
   },
   {
     name: "Tyar En Waka",
     artist: "Jonna Fraser",
     year: 2024,
     duration: 2.41,
+    image: "/img/songs/jonna-fraser-tyar-en-waka.jpg"
   },
   {
     name: "I Need Your Love",
     artist: "Jonna Fraser x Frenna x Yam",
     year: 2024,
     duration: 2.44,
+    image: "/img/songs/jonna-fraser-i-need-your-love.jpg"
   },
   {
     name: "Berichten",
     artist: "Kleine John",
     year: 2024,
     duration: 2.08,
+    image: "/img/songs/kleine-john-berichten.jpg"
   },
   {
     name: "Nog Steeds",
     artist: "KM x Lijpe",
     year: 2023,
     duration: 2.46,
+    image: "/img/songs/km-nog-steeds.jpg"
   },
   {
     name: "Camera",
     artist: "Bryan MG ft. MB, Tawsen",
     year: 2024,
     duration: 2.58,
+    image: "/img/songs/bryan-mg-camera.jpg"
   },
   {
     name: "Guatauba",
     artist: "Plan B",
     year: 2002,
     duration: 2.44,
+    image: "/img/songs/plan-b-guatauba.jpg"
   },
   {
     name: "Carita de Inocente",
     artist: "Prince Royce",
     year: 2020,
     duration: 3.10,
+    image: "/img/songs/prince-royce-carita-de-inocente.jpg"
   },
   {
     name: "Rechazame",
     artist: "Prince Royce",
     year: 2010,
     duration: 3.45,
+    image: "/img/songs/prince-royce-rechazame.jpg"
   },
   {
     name: "Xtra Cool",
     artist: "Young John",
     year: 2022,
     duration: 5.14,
+    image: "/img/songs/young-john-xtra-cool.jpg"
   },
   {
     name: "Yo Voy",
     artist: "Zion & Lennox ft. Daddy Yankee",
     year: 2004,
     duration: 3.51,
+    image: "/img/songs/zion-lennox-yo-voy.jpg"
   },
   {
     name: "WINE",
     artist: "B Young",
     year: 2020,
     duration: 5.48,
+    image: "/img/songs/b-young-wine.jpg"
   },
   {
     name: "Anders Doet",
     artist: "Architrackz ft. Jayh",
     year: 2022,
     duration: 5.39,
+    image: "/img/songs/architrackz-anders-doet.jpg"
   },
   {
     name: "Trop Parler",
     artist: "Franglish",
     year: 2023,
     duration: 3.04,
+    image: "/img/songs/franglish-trop-parler.jpg"
   },
   {
     name: "El Clásico",
     artist: "Lijpe ft. Frenna",
     year: 2022,
     duration: 2.40,
+    image: "/img/songs/lijpe-el-clasico.jpg"
   },
   {
     name: "Hurtin me",
     artist: "Stefflon Don ft. French Montana",
     year: 2017,
     duration: 3.34,
+    image: "/img/songs/stefflon-don-hurtin-me.jpg"
   },
   {
     name: "Sans Effet",
     artist: "Tayc",
     year: 2022,
     duration: 5.21,
+    image: "/img/songs/tayc-sans-effet.jpg"
   },
   {
     name: "Essence",
     artist: "Wizkid ft. Tems",
     year: 2020,
     duration: 4.05,
+    image: "/img/songs/wizkid-essence.jpg"
   },
   {
     name: "Validée",
     artist: "Booba ft. Benash",
     year: 2015,
     duration: 3.26,
+    image: "/img/songs/booba-validee.jpg"
   },
   {
     name: "African Queen",
     artist: "KM ft. Bryan MG",
     year: 2020,
     duration: 2.59,
+    image: "/img/songs/km-african-queen.jpg"
   },
   {
     name: "Envy Us",
     artist: "Stefflon Don ft. Abra Cadabra",
     year: 2016,
     duration: 3.48,
+    image: "/img/songs/stefflon-don-envy-us.jpg"
   },
   {
     name: "Carlo Colucci",
     artist: "Driess",
     year: 2019,
     duration: 2.20,
+    image: "/img/songs/driess-carlo-colucci.jpg"
   },
   {
     name: "Pantat",
     artist: "Kimberlee Ramirez ft. BD, JPB",
     year: 2021,
     duration: 2.19,
+    image: "/img/songs/kimberlee-ramirez-pantat.jpg"
   },
   {
     name: "Leven",
     artist: "Jack ft. Ashafar",
     year: 2019,
     duration: 2.09,
+    image: "/img/songs/jack-leven.jpg"
   }
 ];
 
--- script.js
+++ script.js
@@ -2,10 +2,13 @@ import songs from "./data.js";
 
 function loadSongs() {
   const songList = document.getElementById("songs-list");
+
+  // Loop every song in the list
   songs.forEach((song) => {
+    // Add new row to the table
     const row = document.createElement("tr");
     row.innerHTML = `
-      <td>TODO: INSERT IMAGE</td>
+      <td><img src="${song.image}" alt="${song.name} image" width="80" height="80"></td>
       <td>${song.name}</td>
       <td>${song.artist}</td>
       <td>${song.year}</td>
